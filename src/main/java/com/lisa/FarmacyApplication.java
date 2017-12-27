5a81b9cd41d7237bd8eba990a803f82fd1a1d19a
dimaNCintern <workday2006@gmail.com>
2017-12-27 20:51:51 +0300

    first commit

---
+++ src/main/java/com/lisa/FarmacyApplication.java
@@ -0,0 +1,12 @@
+package com.lisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacyApplication.class, args);
+	}
+}
