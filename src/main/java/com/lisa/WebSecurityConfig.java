f08409fd6fe891fef7f410f0f5e2391168083798
dimaNCintern <workday2006@gmail.com>
2017-12-30 15:28:24 +0300

    permit for /static/css

--- src/main/java/com/lisa/WebSecurityConfig.java
+++ src/main/java/com/lisa/WebSecurityConfig.java
@@ -15,17 +15,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "/home").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .permitAll();
+
+        http.authorizeRequests().antMatchers("/login*").permitAll();
     }
 
     @Autowired
