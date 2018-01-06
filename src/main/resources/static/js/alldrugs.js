800072f16e2e56409fd4184949f04c17f0e994a3
dimaNCintern <workday2006@gmail.com>
2018-01-06 18:55:54 +0300

    add constrain and function for sql script

--- src/main/resources/static/js/alldrugs.js
+++ src/main/resources/static/js/alldrugs.js
@@ -3,7 +3,7 @@
  */
 $(document).ready(function () {
     $.getJSON(
-        '/drug/all',
+        '/drugstore/all',
         function (json) {
             var row_count = 1;
             var table = '<caption>List of drugs in our farmacy</caption>' +
@@ -18,43 +18,11 @@ $(document).ready(function () {
                 '<tbody>' +
                 '<tr>'
             for(var k in json){
-                table += '<th scope="row">'+ (row_count++) + '</th>' + '<td>' + k.drug_name + '</td>' +
-                    '<td>' + k.amount + '</td>' +
-                    '<td>' + k.price+ '</td>' +
+                table += '<th scope="row">'+ (row_count++) + '</th>' + '<td>' + json[k].drug_name + '</td>' +
+                    '<td>' + json[k].amount + '</td>' +
+                    '<td>' + json[k].price+ '</td>' +
                     '</tr>'
             }
             $('table').append(table);
         });
 });
-
-//     <table class="table">
-//     <caption>List of users</caption>
-// <thead>
-// <tr>
-// <th scope="col">#</th>
-// <th scope="col">First</th>
-//     <th scope="col">Last</th>
-//     <th scope="col">Handle</th>
-//     </tr>
-//     </thead>
-//     <tbody>
-//     <tr>
-//     <th scope="row">1</th>
-//     <td>Mark</td>
-//     <td>Otto</td>
-//     <td>@mdo</td>
-// </tr>
-// <tr>
-// <th scope="row">2</th>
-//     <td>Jacob</td>
-//     <td>Thornton</td>
-//     <td>@fat</td>
-// </tr>
-// <tr>
-// <th scope="row">3</th>
-//     <td>Larry</td>
-//     <td>the Bird</td>
-// <td>@twitter</td>
-// </tr>
-// </tbody>
-// </table>
