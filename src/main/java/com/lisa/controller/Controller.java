8d982fad959ea13da47da7db9a10c821ee93e0ab
dimaNCintern <workday2006@gmail.com>
2018-01-06 16:46:49 +0300

    add constrain and function for sql script

--- src/main/java/com/lisa/controller/Controller.java
+++ src/main/java/com/lisa/controller/Controller.java
@@ -4,10 +4,7 @@ import com.lisa.dao.ClientDao;
 import com.lisa.dao.DrugDao;
 import com.lisa.dao.DrugStroteDao;
 import com.lisa.dao.PharmacistDao;
-import com.lisa.entity.Client;
-import com.lisa.entity.Drug;
-import com.lisa.entity.DrugStore;
-import com.lisa.entity.Pharmacist;
+import com.lisa.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,12 +31,12 @@ public class Controller {
 
 
     @RequestMapping("/client/all")
-    List<Client>getAllClients(){
+    List<Users>getAllClients(){
         return clientDao.getAllClients();
     }
 
     @RequestMapping("/pharmacist/all")
-    List<Pharmacist>getAllPharmacists(){
+    List<Users>getAllPharmacists(){
         return pharmacistDao.getAllPharmacists();
     }
 
--- src/main/java/com/lisa/webconfig/WebSecurityConfig.java
+++ src/main/java/com/lisa/webconfig/WebSecurityConfig.java
@@ -32,6 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         List<Users> userList = pharmacistDao.getAllPharmacists();
+        userList.addAll(clientDao.getAllClients());
 
         userList.forEach(K ->{
             try {
