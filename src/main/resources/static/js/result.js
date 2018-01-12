9e5e6283af8c86e36275f671cbf4b28f08ef5fe5
dimaNCintern <workday2006@gmail.com>
2018-01-12 10:31:22 +0300

    event

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -1,9 +1,68 @@
 function result() {
     $(document).ready(function(){
-        alert("result1");
         $("#customerForm").submit(function(event) {
             event.preventDefault();
-            alert("tettete");
+            $.ajax({
+                type : "POST",
+                contentType : "application/json",
+                url : window.location +"/sale",
+                data : $('#customerForm').serialize(),
+                dataType : 'json',
+                success : function(json) {
+                    $('#customerForm').empty();
+                    var row_count = 1;
+                    var table = '<caption>List of drugs in our farmacy</caption>' +
+                        '<thead>' +
+                        '<tr>' +
+                        '<th scope="col">#</th>' +
+                        '<th scope="col">Number of order</th>' +
+                        '<th scope="col">Client</th>' +
+                        '<th scope="col">Pharmacist</th>' +
+                        '<th scope="col">Drug\'s name</th>' +
+                        '<th scope="col">Amount</th>' +
+                        '<th scope="col">Price</th>' +
+                        '<th scope="col">Summ</th>'+
+                        '</tr>' +
+                        '</thead>' +
+                        '<tbody>'
+                    for(var k in json){
+                        table += '<tr><th scope="row">'+ (row_count++) + '</th>' +
+                            '<td>' + json[k].id_order + '</td>' +
+                            '<td>' + json[k].clientName + '</td>'
+                        if(json[k].pharmasyName == 'default'){
+                            table +='<td>' + '</td>'
+                        }else{
+                            table +='<td>' + json[k].pharmasyName+ '</td>'
+                        }
+
+                        var map = json[k].sells;
+                        var count = 0;
+                        var summ = 0;
+                        for(i in map){
+                            if (count !=0 ){
+                                summ += map[i].key * map[i].value;
+                                table +='</tr><tr><td></td><td></td><td></td><td></td>'+'<td>' +i.split(/'/)[1] + '<td>' +map[i].key+ '</td><td>' +map[i].value +'</td><td>'+(map[i].key * map[i].value)+'</td>';
+                            }else{
+                                summ += map[i].key * map[i].value;
+                                table +='<td>' +i.split(/'/)[1] + '<td>' +map[i].key+ '</td><td>' +map[i].value +'</td><td>'+(map[i].key * map[i].value)+'</td>';
+                            }
+
+                            if(count == Object.keys(map).length - 1){
+                                table +='</tr><tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>' +summ + '</td>';
+                            }
+                            count++;
+                        }
+
+                        table +='</tr>'
+                    }
+                    table += '</tbody>'
+                    $('#customerForm').append(table);
+                },
+                error : function(e) {
+                    alert("Error!")
+                    console.log("ERROR: ", e);
+                }
+            });
         });
     });
 
